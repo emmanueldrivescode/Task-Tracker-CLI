825d7adfedc1b5dbcc67857b4c33b1ddaf37cbdc
Emmanuel Dairo <ayomidedairo194@gmail.com>
2026-07-03 22:29:02 +0100

    main.py Updated

--- main.py
+++ main.py
@@ -14,76 +14,76 @@ if len(sys.argv) < 2:
     sys.exit()
 
     # A variable used to acces the CLI prompt through indexing
-    command = sys.argv[1]
+command = sys.argv[1]
 
-    # -------- ADD -----------
-    if command == "add":
-        if len(sys.argv) < 3:
-            print('Usage: python main.py add "Task Description"')
-        else:
-            description = sys.argv[2]
-            add_task(description)
+# -------- ADD -----------
+if command == "add":
+    if len(sys.argv) < 3:
+        print('Usage: python main.py add "Task Description"')
+    else:
+        description = sys.argv[2]
+        add_task(description)
 
- # ------------- LIST -------------
-    elif command == "list":
+# ------------- LIST -------------
+elif command == "list":
+    
+    if len(sys.argv) == 2:
+        list_tasks()
+    elif len(sys.argv) == 3:
+        status = sys.argv[2]
         
-        if len(sys.argv) == 2:
-            list_tasks()
-        elif len(sys.argv) == 3:
-            status = sys.argv[2]
-            
-            if status not in ['todo', 'in-progress', 'done']:
-                print("Invalid status. Choose from: 'done', 'todo', 'in-progress'")
-            else:
-                list_tasks_by_status(status)
+        if status not in ['todo', 'in-progress', 'done']:
+            print("Invalid status. Choose from: 'done', 'todo', 'in-progress'")
         else:
-            print("Usage:")
-            print("python main.py list")
-            print("python main.py list <todo|in-progress|done>")
+            list_tasks_by_status(status)
+    else:
+        print("Usage:")
+        print("python main.py list")
+        print("python main.py list <todo|in-progress|done>")
 
- # --------------- DELETE ----------------
-    elif command == "delete":
-        if len(sys.argv) < 3:
-            print("Usage: python main.py delete <id>")
-        else:
-            try:
-                task_id = int(sys.argv[2])
-                delete_task(task_id)
-            except ValueError:
-                print("Task ID must be a number")
+# --------------- DELETE ----------------
+elif command == "delete":
+    if len(sys.argv) < 3:
+        print("Usage: python main.py delete <id>")
+    else:
+        try:
+            task_id = int(sys.argv[2])
+            delete_task(task_id)
+        except ValueError:
+            print("Task ID must be a number")
 
- # ----------------- UPDATE --------------------
-    elif command == "update":
-        if len(sys.argv) < 4:
-            print('Usage: python main.py update <id> "New Description"')
-        else:
-            try:
-                task_id = int(sys.argv[2])
-                description = sys.argv[3]
-                update_task(task_id, description)
-            except ValueError:
-                print("Task ID must be a number.")
+# ----------------- UPDATE --------------------
+elif command == "update":
+    if len(sys.argv) < 4:
+        print('Usage: python main.py update <id> "New Description"')
+    else:
+        try:
+            task_id = int(sys.argv[2])
+            description = sys.argv[3]
+            update_task(task_id, description)
+        except ValueError:
+            print("Task ID must be a number.")
 
- # ------------------ MARK-IN-PROGRESS-------------------
-    elif command == "mark-in-progress":
-        if len(sys.argv) < 3:
-            print("Usage: python main.py mark-in-progress <id>")
-        else:
-            try:
-                task_id = int(sys.argv[2])
-                mark_in_progress(task_id)
-            except ValueError:
-                print("Task ID must be a number.")
- # ----------------- MARK-DONE------------------
-    elif command == "mark-done":
-        if len(sys.argv) < 3:
-            print("Usage: python main.py mark-done <id>")
-        else:
-            try:
-                task_id = int(sys.argv[2])
-                mark_done(task_id)
-            except ValueError:
-                print("Task ID must be a number.")
+# ------------------ MARK-IN-PROGRESS-------------------
+elif command == "mark-in-progress":
+    if len(sys.argv) < 3:
+        print("Usage: python main.py mark-in-progress <id>")
+    else:
+        try:
+            task_id = int(sys.argv[2])
+            mark_in_progress(task_id)
+        except ValueError:
+            print("Task ID must be a number.")
+# ----------------- MARK-DONE------------------
+elif command == "mark-done":
+    if len(sys.argv) < 3:
+        print("Usage: python main.py mark-done <id>")
+    else:
+        try:
+            task_id = int(sys.argv[2])
+            mark_done(task_id)
+        except ValueError:
+            print("Task ID must be a number.")
 else:
     print("Invalid command!")
     print()
